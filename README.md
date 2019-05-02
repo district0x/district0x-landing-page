79f0692cb3a86349413c23deb8980dd163c6bf38
Filip Bielejec <fbielejec@gmail.com>
2019-05-02 14:22:50 +0200

    Feature/deployment (#36)
    * deployment

---
+++ README.md
@@ -0,0 +1,3 @@
+# [district0x](https://district0x.io/)
+
+[![Build Status](https://travis-ci.org/district0x/district0x-landing-page.svg?branch=master)](https://travis-ci.org/district0x/district0x-landing-page)
---
+++ docker-push.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function login {
+  echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+}
+
+TAG=$(git log -1 --pretty=%h)
+
+docker build -t district0x/district0x-landing-page:$TAG .
+docker tag district0x/district0x-landing-page:$TAG district0x/district0x-landing-page:latest
+
+login
+docker push district0x/district0x-landing-page
